9596438708717fa2df8400503a2b5cad7347b827
lnyan <lkwq007@gmail.com>
2022-10-01 20:10:21 +0800

    Remove shadow root

--- js/mode.js
+++ js/mode.js
@@ -1,8 +1,6 @@
 function(mode){
-    let app=document.querySelector("gradio-app").shadowRoot;
-    console.log(app);
+    let app=document.querySelector("gradio-app");
     let frame=app.querySelector("#sdinfframe").contentWindow;
-    console.log(frame);
     frame.postMessage(["mode", mode], "*");
     return mode;
 }
