ed330fa1772cf08567f929d621af9e0d735c20e6
lnyan <lkwq007@gmail.com>
2022-10-01 17:28:53 +0800

    Update

--- app.py
+++ app.py
@@ -195,6 +195,9 @@ function (x)
     console.log(document.querySelector("gradio-app"));
     console.log(document.querySelector("iframe"));
     console.log(document.querySelector("#sdinfframe").contentWindow);
+    console.log(document.querySelector("#sdinfframe").contentWindow.document);
+    console.log(document.querySelector("#sdinfframe").contentWindow.document).querySelector("#{name}");
+    console.log(document.querySelector("#sdinfframe").contentDocument);
     let frame=document.querySelector("gradio-app").shadowRoot.querySelector("#sdinfframe").contentWindow.document;
     let button=frame.querySelector("#{name}");
     button.click();
