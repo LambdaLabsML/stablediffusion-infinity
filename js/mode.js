bf734d9a0f47897a643c48d428e2c29b58e97963
lnyan <lkwq007@gmail.com>
2022-10-01 20:00:18 +0800

    Update

--- js/mode.js
+++ js/mode.js
@@ -1,6 +1,8 @@
 function(mode){
     let app=document.querySelector("gradio-app").shadowRoot;
+    console.log(app);
     let frame=app.querySelector("#sdinfframe").contentWindow;
+    console.log(frame);
     frame.postMessage(["mode", mode], "*");
     return mode;
 }
